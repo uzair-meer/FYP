95463251dedc4dbdde47409fb467e118968b97b5
hamza-choudhary <humzach700@gmail.com>
2023-10-01 12:05:20 +0500

    add select-company route with functionality

--- backend/src/api/controllers/client.controller.js
+++ backend/src/api/controllers/client.controller.js
@@ -32,7 +32,67 @@ export async function postBooking(req, res, next) {
 
 		res.status(200).json({
 			status: 'ok',
-			data: { ...result._doc }, //_doc contains that data freshly entered in db
+			data: result._doc, //_doc contains that data freshly entered in db
+		})
+	} catch (error) {
+		next(error)
+	}
+}
+
+export async function getCompanyWithPrices(req, res, next) {
+	const inventoryItems = req.query.inventoryItems.split(',')
+
+	try {
+		const result = await Inventory.aggregate([
+			{
+				$sort: { createdAt: -1 }, // Sort by createdAt in descending order to get the latest document
+			},
+			{
+				$group: {
+					_id: '$companyId', // Group by companyId
+					latestInventory: { $first: '$$ROOT' }, // Get the first document in each group (the latest)
+				},
+			},
+			{
+				$lookup: {
+					from: 'users', // The name of the User collection
+					localField: '_id', //this is the _id in group stage as mongo wants every document uniquly identify so we have to assign new _id as we are removing preious one by grouping
+					foreignField: '_id',
+					as: 'user',
+				},
+			},
+			{
+				$project: {
+					_id: 0, // Exclude _id field from the result
+					companyId: '$_id',
+					companyName: { $arrayElemAt: ['$user.name', 0] }, // Get the companyName from the user field
+					inventory: {
+						$map: {
+							input: {
+								$filter: {
+									input: '$latestInventory.inventory',
+									as: 'item',
+									cond: {
+										$in: ['$$item.name', inventoryItems], //it want its second argument to be a array to select only items whose values match
+									},
+								},
+							},
+							as: 'item',
+							in: {
+								name: '$$item.name',
+								movingPrice: '$$item.movingPrice',
+								packingPrice: '$$item.packingPrice',
+								unpackingPrice: '$$item.unpackingPrice',
+							},
+						},
+					},
+				},
+			},
+		])
+
+		res.status(200).json({
+			status: 'ok',
+			data: result,
 		})
 	} catch (error) {
 		next(error)
--- backend/src/api/routes/client.routes.js
+++ backend/src/api/routes/client.routes.js
@@ -1,10 +1,24 @@
 import express from 'express'
-import { postBooking } from '../controllers/client.controller.js'
+import {
+	getCompanyWithPrices,
+	postBooking,
+} from '../controllers/client.controller.js'
 
 const router = express.Router()
 
-const route = "/client"
+const route = '/client'
 
 router.post(`${route}/booking`, postBooking)
+router.get(`${route}/select-company`, getCompanyWithPrices)
 
 export { router as clientRoutes }
+
+
+//FIXME: add this middle ware in before getCompanyWithPrices
+// Middleware to check if either products or services are missing
+// const validateQueryParams = (req, res, next) => {
+//   if (!req.query.products && !req.query.services) {
+//     return next(new Error('Both products and services are missing.'));
+//   }
+//   next();
+// };
