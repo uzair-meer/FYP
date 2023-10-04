611ef8fcc1404429349d11405cb082549d877076
uzair-meer <miruzair038@gmail.com>
2023-10-04 22:25:08 +0500

    added backend

--- backend/routes/companyroute.js
+++
@@ -1,9 +0,0 @@
-import  express  from "express";
-import CompanyController from "../controllers/CompanyController.js";
-
-const Companyrouter  = express.Router()
-Companyrouter.post('/companies/register', CompanyController.registerCompany)
-
-Companyrouter.get('/companies', CompanyController.getAllCompanies)
-
-export default Companyrouter
