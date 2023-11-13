8ebf9be38c65970a1dd9e30ce769ae62b56ffffa
hamza-choudhary <humzach700@gmail.com>
2023-11-14 04:46:23 +0500

    maps added of leafletjs

--- frontend/src/pages/driver/dashboard/DriverDashboard.jsx
+++ frontend/src/pages/driver/dashboard/DriverDashboard.jsx
@@ -1,52 +1,46 @@
 import { useEffect } from 'react'
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:5000') // Use environment variable for server URL
-const updateInterval = 10000 // Location update frequency in milliseconds
+const socket = io('http://localhost:5000')
+const updateInterval = 5000
 
 export default function DriverDashboard() {
 	useEffect(() => {
 		socket.emit('joinRoom', { roomId: '123' })
 
-		let intervalId
+		const emitLocation = (coords) => {
+			const location = { lat: coords.latitude, lng: coords.longitude }
+			console.log('Emitting location:', location) // Debugging log
+			socket.emit('updateLocation', { location, roomId: '123' })
+		}
+
 		const watchId = navigator.geolocation.watchPosition(
 			({ coords }) => {
-				// Function to emit location
-				const emitLocation = () => {
-					const location = { lat: coords.latitude, lng: coords.longitude }
-					socket.emit('updateLocation', { location, roomId: '123' })
-				}
-
-				emitLocation() // Emit immediately on watch start
-				intervalId = setInterval(emitLocation, updateInterval) // Set interval for repeated updates
+				emitLocation(coords) // Emit immediately on watch start
 			},
 			(err) => console.error(err),
 			{ enableHighAccuracy: true }
 		)
 
+		const intervalId = setInterval(() => {
+			navigator.geolocation.getCurrentPosition(
+				({ coords }) => emitLocation(coords),
+				(err) => console.error(err),
+				{ enableHighAccuracy: true }
+			)
+		}, updateInterval)
+
 		return () => {
-			navigator.geolocation.clearWatch(watchId)
-			clearInterval(intervalId) // Clear the interval on unmount
+			// console.log('discoonect')
+			// navigator.geolocation.clearWatch(watchId)
+			// clearInterval(intervalId)
+			// socket.disconnect()
 		}
 	}, [])
-
-	// Implement a method to stop tracking
-	// const stopTracking = () => {
-	// 	navigator.geolocation.clearWatch(watchId)
-	// 	clearInterval(intervalId)
-	// 	// Additional logic to notify server/client
-	// }
 
 	return (
 		<div>
 			<h1>Driver Dashboard</h1>
-			{/* <button onClick={stopTracking}>Stop Tracking</button> */}
-			{/* Optionally add UI to change update frequency */}
-			{/* <input
-                type="number"
-                value={updateInterval}
-                onChange={(e) => setUpdateInterval(Number(e.target.value))}
-            /> */}
 		</div>
 	)
 }
