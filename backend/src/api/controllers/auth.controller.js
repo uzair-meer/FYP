d31e1946d71afc10c6fd7000d5f9bd7b536f5ad4
uzair-meer <miruzair038@gmail.com>
2023-11-22 22:16:13 +0500

    added update userprofile and fixed some ui chnages

--- backend/src/api/controllers/auth.controller.js
+++ backend/src/api/controllers/auth.controller.js
@@ -1,102 +1,119 @@
-import bcrypt from 'bcrypt'
-import jwt from 'jsonwebtoken'
-import Company from '../models/Company.model.js'
-import User from '../models/User.model.js'
-import createError from '../utils/createError.js'
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Company from "../models/Company.model.js";
+import User from "../models/User.model.js";
+import createError from "../utils/createError.js";
 
 //FIXME: register user and register company both cases should be handeled in this single api
 export const registerUser = async (req, res, next) => {
-	try {
-		//FIXME: use hash passwords
-		// const hash = bcrypt.hashSync(req.body.password, 5);
-		const { name, email, password, phone, role } = req.body
+  try {
+    //FIXME: use hash passwords
+    // const hash = bcrypt.hashSync(req.body.password, 5);
+    const { name, email, password, phone, role } = req.body;
 
-		let cnic = role !== "company" ? req.body.cnic : undefined
+    let cnic = role !== "company" ? req.body.cnic : undefined;
 
-		const user = new User({
-			name,
-			email,
-			password,
-			phone,
-			cnic,
-			role,
-		})
+    const user = new User({
+      name,
+      email,
+      password,
+      phone,
+      cnic,
+      role,
+    });
 
-		await user.save()
+    await user.save();
 
-		const { _id } = user._doc
+    const { _id } = user._doc;
 
-		if (role === 'company') {
-			const company = new Company({
-				_id,
-				ntn: req.body.ntn
-			}) // we can get addionatl info from front end and add it here status will be requested by default
-			await company.save()
-		}
+    if (role === "company") {
+      const company = new Company({
+        _id,
+        ntn: req.body.ntn,
+      }); // we can get addionatl info from front end and add it here status will be requested by default
+      await company.save();
+    }
 
-		res.status(201).json({ message: 'User has been created.' })
-	} catch (err) {
-		//FIXME:by looking error we can check if email is not same or type of and appropriate response
-		next(err)
-	}
-}
+    res.status(201).json({ message: "User has been created." });
+  } catch (err) {
+    //FIXME:by looking error we can check if email is not same or type of and appropriate response
+    next(err);
+  }
+};
 
 export const login = async (req, res, next) => {
-	try {
-		const user = await User.findOne({ email: req.body.email })
+  try {
+    const user = await User.findOne({ email: req.body.email });
 
-		if (!user) return next(createError(404, 'User not found!'))
+    if (!user) return next(createError(404, "User not found!"));
 
-		//FIXME: hashed pswds should match
-		// const isCorrect = bcrypt.compareSync(req.body.password, user.password)
-		const isCorrect = req.body.password === user.password
+    //FIXME: hashed pswds should match
+    // const isCorrect = bcrypt.compareSync(req.body.password, user.password)
+    const isCorrect = req.body.password === user.password;
 
-		if (!isCorrect) return next(createError(400, 'Wrong password or username!'))
+    if (!isCorrect)
+      return next(createError(400, "Wrong password or username!"));
 
-		//check if company status is not approved then throw error
-		if (user.role === 'company') {
-			const company = await Company.findOne({ _id: user._id })
+    //check if company status is not approved then throw error
+    if (user.role === "company") {
+      const company = await Company.findOne({ _id: user._id });
 
-			if (company.status !== 'approved')
-      //! FIXME: important show proper error in catch block and handle them on frontend
-				return next(
-					createError(
-						400,
-						`company cannot be login because its status is ${company.status}`
-					)
-				)
-		}
+      if (company.status !== "approved")
+        //! FIXME: important show proper error in catch block and handle them on frontend
+        return next(
+          createError(
+            400,
+            `company cannot be login because its status is ${company.status}`
+          )
+        );
+    }
 
-		const token = jwt.sign(
-			{
-				id: user._id,
-			},
-			process.env.JWT_KEY,
-			{ expiresIn: '60d' }
-		)
-		const { password, ...info } = user._doc
-		// console.log("user ID : ", req.userId)
-		res
-			.cookie('access_token', token, {
-				httpOnly: true,
-			})
-			.status(200)
-			.send(info)
-	} catch (err) {
-		next(err)
-	}
-}
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_KEY,
+      { expiresIn: "60d" }
+    );
+    const { ...info } = user._doc;
+    // console.log("user ID : ", req.userId)
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .status(200)
+      .send(info);
+  } catch (err) {
+    next(err);
+  }
+};
 
 export const logout = async (req, res) => {
-	res
-		.clearCookie('access_token', {
-			sameSite: 'none',
-			secure: true,
-		})
-		.status(200)
-		.send('User has been logged out.')
-}
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .send("User has been logged out.");
+};
 
+export const updateProfile = async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const updates = req.body;
+
+    // Optionally, validate updates here
+
+    // Find the user and update their profile
+    const updatedUser = await User.findByIdAndUpdate(userId, updates, {
+      new: true,
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 // export const registerCompany = async (req, res, next) => {
 // 	// console.log(req.body)
 // 	//#FIXME role must be in [client, company, employee, admin]
--- backend/src/api/models/Chat.modal.js
+++ backend/src/api/models/Chat.modal.js
@@ -1,39 +1,39 @@
-import mongoose from 'mongoose'
-const Schema = mongoose.Schema
+import mongoose from "mongoose";
+const Schema = mongoose.Schema;
 
 const messageSchema = new Schema({
-	sender: {
-		type: String, //client or company
-    require: true
-	},
-	message: {
-		type: String,
-		require: true,
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-	},
-})
+  sender: {
+    type: String, //client or company
+    require: true,
+  },
+  message: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 const chatSchema = new Schema({
-	clientId: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		require: true,
-	},
-	companyId: {
-		type: Schema.Types.ObjectId,
-		ref: 'User',
-		require: true,
-	},
-	messages: [messageSchema],
-	lastUpdated: {
-		type: Date,
-		default: Date.now,
-	},
-})
+  clientId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  companyId: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    require: true,
+  },
+  messages: [messageSchema],
+  lastUpdated: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const Chat = mongoose.model('Chat', chatSchema)
+const Chat = mongoose.model("Chat", chatSchema);
 
-export default Chat
+export default Chat;
